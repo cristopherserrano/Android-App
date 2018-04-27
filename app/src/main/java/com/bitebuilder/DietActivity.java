69036f216e941ab09ff2a98054f58960511af48c
Cristopher Serrano <cristopherserrano@d-172-25-249-55.dhcp.virginia.edu>
2018-04-26 22:58:12 -0400

    cris

--- app/src/main/java/com/bitebuilder/DietActivity.java
+++ app/src/main/java/com/bitebuilder/DietActivity.java
@@ -36,7 +36,7 @@ public class DietActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+
     }
 
 
